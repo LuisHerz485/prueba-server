4c70808bf67fb7ff729138a577f3d98065a4aa6a
unknown <luis.hernandez.albia@gmail.com>
2022-07-13 17:25:54 -0500

    first commit

---
+++ server/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using server.Models;
+
+namespace server
+{
+    public class ApplicationDbContext : DbContext
+
+    {
+        public DbSet<Alumno> Alumno { get; set;}
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ server/Models/Alumno.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace server.Models
+{
+    public class Alumno
+    {
+       public int Id { get; set; }
+
+        [Required]
+       public string apellidos { get; set; }
+
+        [Required]
+        public string nombres { get; set; }
+
+        [Required]
+        public string dni { get; set; }
+    }
+}
